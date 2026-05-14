07a8b4d160aab7f4fa97e36ff19efad3410448a6
Linhdieu <linh.mtd2416807@sis.hust.edu.vn>
2026-05-14 23:45:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,18 +11,6 @@ vuln = int(os.getenv('vulnerable', 1))
 # token alive for how many seconds?
 alive = int(os.getenv('tokentimetolive', 60))
 
-@app.after_request
-def add_security_headers(response):
-    # Chống trình duyệt tự đoán kiểu dữ liệu
-    response.headers['X-Content-Type-Options'] = 'nosniff'
-    # Ghi đè tên Server để giấu thông tin thật
-    response.headers['Server'] = 'VAmPI-Secure-Server'
-    # Chống gọi tài nguyên chéo từ trang lạ
-    response.headers['Cross-Origin-Resource-Policy'] = 'same-origin'
-    # Ngăn trình duyệt lưu cache dữ liệu nhạy cảm
-    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
-    return response
-
 # start the app with port 5000 and debug on!
 if __name__ == '__main__':
     vuln_app.run(host='0.0.0.0', port=5000, debug=True)

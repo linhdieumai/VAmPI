b97982cce2b3760e22c07eae04167f033dd77daa
Linhdieu <linh.mtd2416807@sis.hust.edu.vn>
2026-06-07 17:58:09 +0700

    Fix SQL injection vulnerability in get_user method
    Refactored SQL query to prevent SQL injection by using parameterized queries.

--- models/user_model.py
+++ models/user_model.py
@@ -70,8 +70,8 @@ class User(db.Model):
     @staticmethod
     def get_user(username):
         if vuln:  # SQLi Injection
-            user_query = f"SELECT * FROM users WHERE username = '{username}'"
-            query = db.session.execute(text(user_query))
+            user_query = "SELECT * FROM users WHERE username = :username"
+            query = db.session.execute(text(user_query), {"username": username})
             ret = query.fetchone()
             if ret:
                 fin_query = '{"username": "%s", "email": "%s"}' % (ret[1], ret[3])
